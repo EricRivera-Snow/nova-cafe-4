68397794507545808fad79527aa88a33438bdc3f
EricRivera-Snow <eric.rivera@aya.yale.edu>
2025-02-10 18:37:06 -0500

    completed nav bar to look like the screenshot

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,44 @@
 import { Link } from "react-router-dom";
+import { FaUtensils } from "react-icons/fa";
 
 function Header() {
   return (
-    <header className="bg-darkGray text-dutchWhite p-4 flex justify-between">
-      <h1 className="text-xl">Nova Cafe</h1>
-      <nav>
-        <ul className="flex gap-4">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/menu">Menu</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/order">Order</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+    <header className="bg-white text-darkGray p-4 fixed top-0 left-0 w-full shadow-md z-50 rounded-b-lg">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <div className="flex items-center space-x-2">
+          <FaUtensils className="text-xl" />
+          <h1 className="text-xl font-bold">Nova Cafe</h1>
+        </div>
+        <nav>
+          <ul className="flex gap-6 text-lg">
+            <li>
+              <Link to="/" className="hover:text-blueWave">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/menu" className="hover:text-blueWave">
+                Menu
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="hover:text-blueWave">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/order" className="hover:text-blueWave">
+                Order
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="hover:text-blueWave">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
