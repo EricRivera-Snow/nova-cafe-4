2cbceeb630592b100ec19d03a8a8403b13a479c3
Greg Kurka <gregkurka@gmail.com>
2025-02-10 18:17:56 -0600

    updated footer height

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,51 +7,51 @@ import {
 
 const Footer = () => {
   return (
-    <footer className="bg-white text-gray-800 py-8 fixed bottom-0 w-full shadow-top">
-      <div className="max-w-7xl mx-auto px-6 lg:px-8">
-        <div className="flex flex-col md:flex-row justify-between items-center">
-          {/* Contact Info */}
-          <div className="text-center md:text-left">
-            <h2 className="text-lg font-semibold">Contact Us</h2>
-            <p className="mt-2">312 E Main St, Bozeman, MT 59715</p>
-            <p>(406) 587-3973</p>
-          </div>
-
-          {/* Social Media Icons */}
-          <div className="flex space-x-6 mt-4 md:mt-0">
-            <a
-              href="https://www.facebook.com/NovaCafe/"
-              className="text-gray-600 hover:text-gray-800"
-            >
-              <FontAwesomeIcon icon={faFacebook} size="lg" />
-            </a>
-            <a
-              href="https://x.com/novacafe"
-              className="text-gray-600 hover:text-gray-800"
-            >
-              <FontAwesomeIcon icon={faTwitter} size="lg" />
-            </a>
-            <a
-              href="https://www.instagram.com/thenovacafe/?hl=en"
-              className="text-gray-600 hover:text-gray-800"
-            >
-              <FontAwesomeIcon icon={faInstagram} size="lg" />
-            </a>
-          </div>
+    <footer className="bg-white text-gray-800 py-4 fixed bottom-0 w-full shadow-top text-sm">
+      <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
+        {/* Contact Info */}
+        <div className="text-center md:text-left">
+          <h2 className="text-base font-semibold">Contact Us</h2>
+          <p>312 E Main St, Bozeman, MT 59715</p>
+          <p>(406) 587-3973</p>
         </div>
 
-        {/* Bottom Links */}
-        <div className="border-t border-gray-300 mt-6 pt-4 text-center text-sm text-gray-500">
-          <a href="#" className="hover:text-gray-800">
-            Privacy Policy
-          </a>{" "}
-          <a href="#" className="mx-4 hover:text-gray-800">
-            Terms of Service
-          </a>{" "}
-          <a href="#" className="hover:text-gray-800">
-            Help
+        {/* Social Media Icons */}
+        <div className="flex space-x-4 mt-2 md:mt-0">
+          <a
+            href="https://www.facebook.com/NovaCafe/"
+            className="text-gray-600 hover:text-gray-800"
+          >
+            <FontAwesomeIcon icon={faFacebook} size="sm" />
+          </a>
+          <a
+            href="https://x.com/novacafe"
+            className="text-gray-600 hover:text-gray-800"
+          >
+            <FontAwesomeIcon icon={faTwitter} size="sm" />
+          </a>
+          <a
+            href="https://www.instagram.com/thenovacafe/?hl=en"
+            className="text-gray-600 hover:text-gray-800"
+          >
+            <FontAwesomeIcon icon={faInstagram} size="sm" />
           </a>
         </div>
+      </div>
+
+      {/* Bottom Links */}
+      <div className="border-t border-gray-300 mt-2 pt-2 text-center text-xs text-gray-500">
+        <a href="#" className="hover:text-gray-800">
+          Privacy Policy
+        </a>
+        <span className="mx-2">|</span>
+        <a href="#" className="hover:text-gray-800">
+          Terms of Service
+        </a>
+        <span className="mx-2">|</span>
+        <a href="#" className="hover:text-gray-800">
+          Help
+        </a>
       </div>
     </footer>
   );
